d25a8b4e7c2c2b0411505986b8ca45000936890c
SUMITA TAKAHASHI <sumi.5.goo@gmail.com>
2019-02-08 18:21:12 +0900

    変数名変更

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,18 +1,18 @@
 $(function() {
-  var user_list = $("#user-search-result");
-  var member_list = $("#member-search-result");
+  var user_to_search_result = $("#user-search-result");
+  var member_to_search_result = $("#member-search-result");
 
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${ user.name }</p>
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                </div>`;
-    user_list.append(html);
+    user_to_search_result.append(html);
   }
 
   function appendNoUser(user) {
     var html = ``
-    user_list.append(html);
+    user_to_search_result.append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
@@ -35,19 +35,18 @@ $(function() {
         alert('ユーザー検索に失敗しました');
       })
     $(document).on("click", ".user-search-add", function() {
-      var id = $(this).data('user-id');
-      var name = $(this).data('user-name');
+      var user_id = $(this).data('user-id');
+      var user_name = $(this).data('user-name');
       $(this).parent().remove();
       var memberHtml = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                          <input name='group[user_ids][]' type='hidden' value='${id}'>
-                          <p class='chat-group-user__name'>${name}</p>
+                          <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                          <p class='chat-group-user__name'>${user_name}</p>
                           <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                         </div>`;
-      member_list.append(memberHtml);
+      member_to_search_result.append(memberHtml);
     });
   });
   $(document).on("click", ".user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn", function() {
     $(this).parent().remove();
   });
 })
-
